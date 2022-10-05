d35f17bc059ce2bab21882e8baa670921aed4886
mamedov-ma <mamedov.ma@phystech.edu>
2022-10-05 13:46:27 +0300

    add resizing

--- Stack/include/mystack.h
+++ Stack/include/mystack.h
@@ -9,26 +9,30 @@ template <typename T>
 class MyStack
 {
     public:
-        
-        const size_t DEFAULT_STACK_SIZE = 100;
 
         MyStack();
-        MyStack(size_t capacity);
+        explicit MyStack(size_t capacity);
         MyStack(const MyStack& other);
         MyStack(MyStack&& other);
+        ~MyStack();
 
         const MyStack& operator=(const MyStack& other);
         const MyStack& operator=(MyStack&& other);
 
         const T& top() const;
-
         void push(T);
         void pop();
 
         bool is_empty() const;
         bool is_full() const;
 
-        ~MyStack();
+        size_t get_size() const;
+        size_t get_capacity() const;
+
+    public:
+
+        const size_t DEFAULT_STACK_SIZE = 100;
+        const int RESIZE_COEFF = 2;
 
     private:
 
@@ -86,7 +90,10 @@ const MyStack<T>& MyStack<T>::operator=(const MyStack<T>& other)
 template <typename T>
 const MyStack<T>& MyStack<T>::operator=(MyStack<T>&& other)
 {
- 
+    if (data_ == other.data_)
+    {
+        return *this;
+    }
     size_ = other.size_;
     capacity_ = other.capacity_;
     delete[] data_;
@@ -98,32 +105,48 @@ const MyStack<T>& MyStack<T>::operator=(MyStack<T>&& other)
 template <typename T>
 const T& MyStack<T>::top() const
 {
-    return 0;
-
+    if (!is_empty()) 
+    {
+        return data_[size_ - 1];
+    }
+    exit(EXIT_FAILURE);
 }
 
 template <typename T>
-void MyStack<T>::push(T)
+void MyStack<T>::push(T node)
 {
-
+    if (is_full())
+    {
+        capacity_ *= RESIZE_COEFF;
+        T* allocated_memory = new T[capacity_];
+        std::copy(data_, data_ + size_, allocated_memory);
+        delete[] data_;
+        data_ = allocated_memory;  
+    }
+    data_[size_] = node;
+    size_++;
 }
 
 template <typename T>
 void MyStack<T>::pop()
 {
-
+    if (!is_empty())
+    {
+        size_--;
+    }
+    exit(EXIT_FAILURE);
 }
 
 template <typename T>
 bool MyStack<T>::is_empty() const
 {
-    return 0;
+    return size_ == 0;
 }
     
 template <typename T>
 bool MyStack<T>::is_full() const
 {
-    return 0;
+    return size_ == capacity_;
 }
 
 template <typename T>
